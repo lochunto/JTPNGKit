58ba71ff998bce14707454133d107cb944570296
lochunto <lochunto@gmail.com>
2015-12-23 17:04:55 +0800

    Initial Commit

---
+++ JTPNGKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JTPNGKit
+//
+//  Created by John Lo on 15/12/23.
+//  Copyright © 2015年 John Lo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
