d0cd386658c4975d75de2caff991b999a8f7c0f6
lochunto <lochunto@gmail.com>
2016-06-15 17:52:10 +0800

    commit

--- JTPNGKit/main.m
+++ JTPNGKit/main.m
@@ -12,7 +12,6 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
-        NSLog(@"Hello, World!");
     }
     return 0;
 }
